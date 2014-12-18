a065d1a8693cea59e1d5950e58760ae58b44edae
codup <wowas18@nm.ru>
2014-12-18 17:22:46 +0400

    add description

--- l10n_tm/__openerp__.py
+++ l10n_tm/__openerp__.py
@@ -28,6 +28,7 @@ This is the base module to manage the accounting chart for Turkmenistan in OpenE
     """,
     'author': 'CodUP',
     'website': 'http://codup.com',
+    'images': ['static/description/icon.png'],
     'depends': ['account', 'account_chart'],
     'demo': [],
     'data': [
