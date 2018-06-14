f9308a925e5102d31516f08537ab06bfb9855c2f
wowas18 <wowas18@nm.ru>
2018-06-14 17:13:03 +0400

    fix encoding

--- l10n_kz/__manifest__.py
+++ l10n_kz/__manifest__.py
@@ -1,27 +1,14 @@
-﻿# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2014-2016 CodUP (<http://codup.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    Copyright (C) 2014-2018 CodUP (<http://codup.com>).
 #
 ##############################################################################
 
 {
     'name': 'Kazakhstan - Accounting',
-    'version': '3.0',
+    'version': '3.1',
     'category': 'Localization',
     'description': """
 This is the base module to manage the accounting chart for Kazakhstan in OpenERP.
@@ -39,4 +26,3 @@ This is the base module to manage the accounting chart for Kazakhstan in OpenERP
     'sequence': 1,
     'installable': True,
 }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
