ca7358e225f7a8d33e686dc377add045b2f38c7d
wowas18 <wowas18@nm.ru>
2018-11-13 19:09:11 +0400

    change version

--- l10n_uz/__manifest__.py
+++ l10n_uz/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'Uzbekistan - Accounting',
-    'version': '3.1',
+    'version': '3.2',
     'category': 'Localization',
     'description': """
 This is the base module to manage the accounting chart for Uzbekistan in Odoo.
